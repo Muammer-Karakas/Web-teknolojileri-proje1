2d3a5889f95a07edde6bcf9f690d09a14dc6ff1c
Muammer-Karakas <muammerkarakas8@gmail.com>
2022-05-13 01:25:22 +0300

    Web proje aşama7
    iletişim sayfasının html ve php kısmına eklemeler yapıldı.

---
+++ Web sitesi/iletişim.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($_GET['buton'])){
+ $a1 = $_GET['ad'];
+ $a2 = $_GET['soyad'];
+ $a3 = $_GET['kul_adı'];
+ $a4 = $_GET['email'];
+ $a5 = $_GET['gun'];
+ $a6 = $_GET['ay'];
+ $a7 = $_GET['yıl'];
+$a7 = $_GET['dyeri'];
+$a8 = $_GET['cinsiyet'];
+
+echo "Bilgiler güncellendi.."
+echo "$a1"" ""$a2";
+echo "--------";
+echo "$a3";
+echo "$a4";
+echo "$a5""""$a6""""$a7";
+echo "--------";
+echo "$a8";
+}
+
+else{
+ 
+}
+ 
+?>
